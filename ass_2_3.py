121e3bc5c796ff53ec77172a6feaddfd85f90a41
olafuyi <solafuyi@gmail.com>
2021-03-24 11:33:17 +0000

    Add files via upload

---
+++ ass_2_3.py
@@ -0,0 +1,6 @@
+hrs = input ('Enter hours:')
+rate = input ('Enter Rate:')
+rate_per_hr= float(hrs)*float(rate)
+print('Pay: ' + str(rate_per_hr))
+print('Pay: ', str(rate_per_hr))
+print('Pay: ', rate_per_hr )
